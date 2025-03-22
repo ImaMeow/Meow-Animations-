679a6d07a245af779cad6df1cce15912d47bd34f
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-03-22 15:24:02 -0400

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,8 @@
+_G.rightarm = "Accessory (RArm)"
+_G.leftarm = "Accessory (LArm)"
+_G.rightleg = "RARM"
+_G.leftleg = "LARM"
+_G.torso = "Black"
+_G.limbsoffset = CFrame.Angles(0,0,math.rad(-90))
+_G.permdeath = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ImaMeow/Meow-Animations-/refs/heads/main/mainscript.lua"))()
