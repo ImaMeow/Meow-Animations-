06c7b389dbbb6aa53edd3c8ababbba031bd2a65c
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-03-24 06:59:22 -0400

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -81,6 +81,34 @@ fling = function(humanoid)
 	replicatesignal(game.Players.LocalPlayer.Kill)
 	getgenv().flinging = false
 	highlight:Destroy()
+	spawn(function()
+	while true do
+if getgenv().flinging == false then
+		realchar.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+		realchar[getgenv().rightarm].Handle.CFrame = fakechar["Right Arm"].CFrame * getgenv().limbsoffset
+        realchar[getgenv().rightarm].Handle.Velocity = Vector3.new(0,6400,0)
+		realchar[getgenv().leftarm].Handle.CFrame = fakechar["Left Arm"].CFrame * getgenv().limbsoffset
+        realchar[getgenv().leftarm].Handle.Velocity = Vector3.new(0,6400,0)
+		realchar[getgenv().rightleg].Handle.CFrame = fakechar["Right Leg"].CFrame * getgenv().limbsoffset
+        realchar[getgenv().rightleg].Handle.Velocity = Vector3.new(0,6400,0)
+		realchar[getgenv().leftleg].Handle.CFrame = fakechar["Left Leg"].CFrame * getgenv().limbsoffset
+        realchar[getgenv().leftleg].Handle.Velocity = Vector3.new(0,6400,0)
+		realchar[getgenv().torso].Handle.CFrame = fakechar["Torso"].CFrame
+        realchar[getgenv().torso].Handle.Velocity = Vector3.new(0,6400,0)
+		for i,v in pairs(realchar:GetDescendants()) do
+			if v:IsA("Accessory") then
+				if v.Name == getgenv().rightarm or v.Name == getgenv().leftarm or v.Name == getgenv().rightleg or v.Name == getgenv().leftleg or v.Name == getgenv().torso then
+					continue
+				else
+					v.Handle.CFrame = fakechar[v.Name].Handle.CFrame
+                    v.Handle.Velocity = Vector3.new(0,6400,0)
+				end
+			end
+		end
+end
+		task.wait()
+	end
+end)
 end
 game.StarterGui:SetCore("SendNotification",{
 	Title = "Meow Animations";
