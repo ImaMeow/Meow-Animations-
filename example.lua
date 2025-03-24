5b47692f654b370e21a5811e3f0ec0f1e8dc36ba
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-03-24 06:31:47 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -4,5 +4,4 @@ getgenv().rightleg = "RARM"
 getgenv().leftleg = "LARM"
 getgenv().torso = "Black"
 getgenv().limbsoffset = CFrame.Angles(0,0,math.rad(-90))
-getgenv().permdeath = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImaMeow/Meow-Animations-/refs/heads/main/mainscript.lua"))()
