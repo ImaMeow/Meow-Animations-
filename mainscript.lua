738cf7222f2acbe5f5e35a8bdeb74b13153b9266
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-03-24 06:31:34 -0400

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,5 +1,6 @@
-game.Players.LocalPlayer.Character.Archivable = true
-local fakechar = game.Players:CreateHumanoidModelFromDescription(game.Players.LocalPlayer.Character.Humanoid.HumanoidDescription,Enum.HumanoidRigType.R6)
+getgenv().flinging = false
+getgenv().lastflinghumanoid = nil
+fakechar = game.Players:CreateHumanoidModelFromDescription(game.Players.LocalPlayer.Character.Humanoid.HumanoidDescription,Enum.HumanoidRigType.R6)
 fakechar.Parent = game.Workspace
 fakechar.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 spawn(function()
@@ -19,20 +20,15 @@ for i,v in pairs(fakechar:GetDescendants()) do
 end
 fakechar.Head.face:Destroy()
 game.Players.LocalPlayer.Character.Parent = fakechar
-local realchar = fakechar[game.Players.LocalPlayer.Name]
+realchar = fakechar[game.Players.LocalPlayer.Name]
 game.Workspace.CurrentCamera.CameraSubject = fakechar
 game.Players.LocalPlayer.Character = fakechar
-if getgenv().permdeath == true then
-	replicatesignal(game.Players.LocalPlayer.ConnectDiedSignalBackend)
-end
+replicatesignal(game.Players.LocalPlayer.ConnectDiedSignalBackend)
 task.wait(game.Players.RespawnTime+.65)
-if getgenv().permdeath == true then
-	replicatesignal(game.Players.LocalPlayer.Kill)
-else
-	realchar.Humanoid:ChangeState(15)
-end
+replicatesignal(game.Players.LocalPlayer.Kill)
 spawn(function()
 	while true do
+if getgenv().flinging == false then
 		realchar[getgenv().rightarm].Handle.CFrame = fakechar["Right Arm"].CFrame * getgenv().limbsoffset
         realchar[getgenv().rightarm].Handle.Velocity = Vector3.new(0,6400,0)
 		realchar[getgenv().leftarm].Handle.CFrame = fakechar["Left Arm"].CFrame * getgenv().limbsoffset
@@ -53,51 +49,34 @@ spawn(function()
 				end
 			end
 		end
+end
 		task.wait()
 	end
 end)
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-	task.wait(.1)
+spawn(function()
+	while true do
+if getgenv().flinging == true then
+		realchar.HumanoidRootPart.CFrame = getgenv().lastflinghumanoid.Parent.HumanoidRootPart.CFrame
+		realchar.HumanoidRootPart.AssemblyAngularVelocity = Vector3.new(6400,6400,6400)
+end
+		task.wait()
+	end
+end)
+fling = function(humanoid)
+	replicatesignal(game.Players.LocalPlayer.ConnectDiedSignalBackend)
+	task.wait(game.Players.RespawnTime+.65)
+	getgenv().lastflinghumanoid = humanoid
+	getgenv().flinging = true
 	game.Players.LocalPlayer.Character.Parent = fakechar
 	realchar = fakechar[game.Players.LocalPlayer.Name]
 	game.Workspace.CurrentCamera.CameraSubject = fakechar
 	game.Players.LocalPlayer.Character = fakechar
-	if getgenv().permdeath == true then
-		replicatesignal(game.Players.LocalPlayer.ConnectDiedSignalBackend)
-	end
+	task.wait(2)
+	replicatesignal(game.Players.LocalPlayer.ConnectDiedSignalBackend)
 	task.wait(game.Players.RespawnTime+.65)
-	if getgenv().permdeath == true then
-		replicatesignal(game.Players.LocalPlayer.Kill)
-	else
-		realchar.Humanoid:ChangeState(15)
-	end
-	spawn(function()
-	    while true do
-		    realchar[getgenv().rightarm].Handle.CFrame = fakechar["Right Arm"].CFrame * getgenv().limbsoffset
-           realchar[getgenv().rightarm].Handle.Velocity = Vector3.new(0,6400,0)
-	    	realchar[getgenv().leftarm].Handle.CFrame = fakechar["Left Arm"].CFrame * getgenv().limbsoffset
-            realchar[getgenv().leftarm].Handle.Velocity = Vector3.new(0,6400,0)
-	    	realchar[getgenv().rightleg].Handle.CFrame = fakechar["Right Leg"].CFrame * getgenv().limbsoffset
-            realchar[getgenv().rightleg].Handle.Velocity = Vector3.new(0,6400,0)
-	    	realchar[getgenv().leftleg].Handle.CFrame = fakechar["Left Leg"].CFrame * getgenv().limbsoffset
-            realchar[getgenv().leftleg].Handle.Velocity = Vector3.new(0,6400,0)
-	    	realchar[getgenv().torso].Handle.CFrame = fakechar["Torso"].CFrame
-            realchar[getgenv().torso].Handle.Velocity = Vector3.new(0,6400,0)
-	    	for i,v in pairs(realchar:GetDescendants()) do
-	    		if v:IsA("Accessory") then
-	    			if v.Name == getgenv().rightarm or v.Name == getgenv().leftarm or v.Name == getgenv().rightleg or v.Name == getgenv().leftleg or v.Name == getgenv().torso then
-	    				continue
-	    			else
-		    			v.Handle.CFrame = fakechar[v.Name].Handle.CFrame
-                     v.Handle.Velocity = Vector3.new(0,6400,0)
-		    		end
-		    	end
-	    	end
-	    	task.wait()
-    	end
-    end)
-end)
-game.Players.LocalPlayer.SimulationRadius = 9999
+	replicatesignal(game.Players.LocalPlayer.Kill)
+	getgenv().flinging = false
+end
 game.StarterGui:SetCore("SendNotification",{
 	Title = "Meow Animations";
 	Text = "Sucessfully Loaded! Made By ImaMeow"
