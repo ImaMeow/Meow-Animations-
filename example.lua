5124816af2d35a1840d6024d071d8ffdb92ca8ba
ImaMeow <100092400+ImaMeow@users.noreply.github.com>
2025-04-13 20:07:27 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -7,7 +7,7 @@ getgenv().rightarm = "Accessory (LimbThree)"
 getgenv().leftarm = "Accessory (LimbFour)"
 getgenv().rightleg = "Accessory (LimbOne)"
 getgenv().leftleg = "Accessory (LimbTwo)"
-getgenv().torso = "DevAwardsGold"
+getgenv().torso = "Accessory (TorsoBlack)"
 getgenv().limbsoffset = CFrame.Angles(0,0,math.rad(-90))
 getgenv().rigsize = 2
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImaMeow/Meow-Animations-/refs/heads/main/mainscript.lua"))()
